c820718f75077c3400c85d1ee6fc8a6c84d09ee8
Wentao Jiang <skyjiangwt.jiang@mail.utoronto.ca>
2019-10-26 17:33:17 -0400

    two PushBox  activities created.

---
+++ phase1/app/src/main/java/com/example/myapplication/PushBoxIntroActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PushBoxIntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push_box_intro);
+    }
+}
