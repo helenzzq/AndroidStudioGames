1c398c45c4889e29d6095ce1e83ea7fd1571ed81
Yutong Dong <yuto.dong@mail.utoronto.ca>
2019-10-31 19:56:05 -0400

    "delete PUSHOBOX, add puke pics"

--- phase1/app/src/main/java/com/example/myapplication/HelpActivity.java
+++ phase1/app/src/main/java/com/example/myapplication/HelpActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication.pushbox.activities;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.myapplication.R;
+import com.example.myapplication.pushbox.activities.PushBoxGameActivity;
 
 public class HelpActivity extends AppCompatActivity implements View.OnClickListener {
     private Button back, start;
