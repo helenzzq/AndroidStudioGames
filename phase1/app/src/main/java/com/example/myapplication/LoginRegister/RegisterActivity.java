e883c2e5bcadadf84fabca2d5f62da611e602ac7
Xinyan <xinyan.he@mail.utoronto.ca>
2019-10-31 22:32:00 -0400

    delete pushbox package

--- phase1/app/src/main/java/com/example/myapplication/LoginRegister/RegisterActivity.java
+++ phase1/app/src/main/java/com/example/myapplication/LoginRegister/RegisterActivity.java
@@ -3,14 +3,31 @@ package com.example.myapplication.LoginRegister;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.myapplication.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button buttonReg;
+    EditText etNameReg;
+    EditText etPasswordReg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etNameReg = (EditText) findViewById(R.id.etNameReg);
+        etPasswordReg = (EditText) findViewById(R.id.etPasswordReg);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
