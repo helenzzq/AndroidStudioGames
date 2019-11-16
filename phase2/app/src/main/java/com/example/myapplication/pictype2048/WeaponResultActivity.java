d29f6bc74d9e3f5677d74424316d6818737ade5e
helenzzq <helenzhu0411@gmail.com>
2019-11-16 00:01:58 -0500

    Delete Useless Package Snake

--- phase2/app/src/main/java/com/example/myapplication/pictype2048/WeaponResultActivity.java
+++ phase2/app/src/main/java/com/example/myapplication/pictype2048/WeaponResultActivity.java
@@ -1,7 +1,5 @@
 package com.example.myapplication.pictype2048;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,8 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.myapplication.math24.Math24Activity;
 import com.example.myapplication.R;
+import com.example.myapplication.math24.Math24Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class WeaponResultActivity extends AppCompatActivity {
 
