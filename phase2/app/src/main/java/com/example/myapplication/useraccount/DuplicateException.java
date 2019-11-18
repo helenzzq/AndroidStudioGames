8a6d3a3a209a7a006399853fc0c901ca66edef31
Xinyan <xinyan.he@mail.utoronto.ca>
2019-11-18 13:20:06 -0500

    Add UserFileSaver and UserManager class

---
+++ phase2/app/src/main/java/com/example/myapplication/useraccount/DuplicateException.java
@@ -0,0 +1,4 @@
+package com.example.myapplication.useraccount;
+
+class DuplicateException extends Throwable {
+}
---
+++ phase2/app/src/main/java/com/example/myapplication/useraccount/NoPassWordException.java
@@ -0,0 +1,4 @@
+package com.example.myapplication.useraccount;
+
+class NoPassWordException extends Throwable {
+}
