8863f58f4b235005894d01d50e39708c4f52cff9
Xinyan <xinyan.he@mail.utoronto.ca>
2019-11-18 13:08:35 -0500

    Implements serializable for User class

--- phase2/app/src/main/java/com/example/myapplication/useraccount/User.java
+++ phase2/app/src/main/java/com/example/myapplication/useraccount/User.java
@@ -1,6 +1,8 @@
 package com.example.myapplication.useraccount;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
 
     private String username;
     private String password;
