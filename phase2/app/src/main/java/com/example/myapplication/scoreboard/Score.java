c1bdec76dbd25aaecaaf19e01509d3339f658b93
Xinyan <xinyan.he@mail.utoronto.ca>
2019-11-20 23:48:09 -0500

    Scoreboard added

--- phase2/app/src/main/java/com/example/myapplication/scoreboard/Score.java
+++ phase2/app/src/main/java/com/example/myapplication/scoreboard/Score.java
@@ -2,7 +2,7 @@ package com.example.myapplication.scoreboard;
 
 import java.io.Serializable;
 
-public class Score implements Comparable<Score>, Serializable {
+public class Score implements Serializable ,Comparable<Score>{
 
     private int score;
 
@@ -29,10 +29,17 @@ public class Score implements Comparable<Score>, Serializable {
     }
 
 
-
+    /**
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
     @Override
-    public int compareTo(Score other) {
-
-        return Integer.compare(this.getScore(),other.getScore());
+    public int compareTo(Score o) {
+        return this.getScore() - o.getScore();
     }
 }
