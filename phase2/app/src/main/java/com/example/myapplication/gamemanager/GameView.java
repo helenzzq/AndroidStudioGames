4610415d1170af5df277964b3e06225e656d5a46
helenzzq <helenzhu0411@gmail.com>
2019-11-21 21:01:17 -0500

    Create and update the interface GameView and Game Manager

---
+++ phase2/app/src/main/java/com/example/myapplication/gamemanager/GameView.java
@@ -0,0 +1,17 @@
+package com.example.myapplication.gamemanager;
+
+public interface GameView {
+
+
+    /**
+     * Update the score in activity
+     * @param score the score of the player
+     */
+    void updateScore(int score);
+
+    /**
+     * Go to the result page 
+     */
+    void showResult();
+
+}
