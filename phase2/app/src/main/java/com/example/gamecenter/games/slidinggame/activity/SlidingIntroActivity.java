7eb6bcbbdacb3170bf3b77c3d4863d2a011c39a2
Keyi Zhang <kya.zhang@mail.utoronto.ca>
2019-12-01 19:51:54 -0500

    Add a button on the intro layout.

--- phase2/app/src/main/java/com/example/gamecenter/games/slidinggame/activity/SlidingIntroActivity.java
+++ phase2/app/src/main/java/com/example/gamecenter/games/slidinggame/activity/SlidingIntroActivity.java
@@ -15,13 +15,22 @@ import static android.content.Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
 
 public class SlidingIntroActivity extends AppCompatActivity {
 
-    
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sliding_intro);
-
+        setBackButton();
     }
 
+    public void setBackButton() {
+        findViewById(R.id.back_2048).setOnClickListener(v -> {
+            Intent i = new Intent(this, SlidingActivity.class);
+            i.addFlags(FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(i);
+            SlidingActivity.getGameTimer().restart();
 
+
+        });
+    }
 }
